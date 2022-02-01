9fdb96db555a2073805a699ca2bb84fb88347579
mmsafari <mmsafariiranian@gmail.com>
2022-02-01 15:52:39 +0330

    setUp base

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import PasswordGenerator from "./pages/PasswordGenerator";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  let routes = (
+    <Routes>
+      <Route path="/" element={<PasswordGenerator />} />
+    </Routes>
   );
+  return <div>{routes}</div>;
 }
 
 export default App;
---
+++ src/pages/PasswordGenerator.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const PasswordGenerator = () => {
+  return (
+    <div className="container">
+      <p className="mainTitle">Password Generator</p>
+      <div className="resultContainer">
+        <span>sdsd</span>
+        <button className="btn" title="Copy">
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M4 2h7a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H4z"
+            />
+            <path d="M6 0h7a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2v-1a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1H4a2 2 0 0 1 2-2z" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PasswordGenerator;
